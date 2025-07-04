dddd6f503f03dae748c8c03f49f11766b5d38b66
Paajanen Roope <roope.paajanen@op.fi>
2025-07-04 10:47:43 +0300

    Using stacks to arrange views

---
+++ Using stacks to arrange views/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Roope Paajanen on 4.7.2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
---
+++ Using stacks to arrange views/UsingStacksToArrangeViews/StartingProject/README.md
@@ -0,0 +1,5 @@
+# Using Stacks to Arrange Views
+
+## Starting Project
+
+Use this project to code along with [Using Stacks to Arrange Views](https://developer.apple.com/tutorials/app-dev-training/using-stacks-to-arrange-views).
